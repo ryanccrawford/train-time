f103061c496d2ec906c32e58d5d8605e94f97a09
RyBo <ryanccrawford@live.com>
2019-04-27 22:09:41 -0400

    Most code is complete. Mostly commiting for backing up

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# train-time
+## Train Time
 Trains Trains Trains 
+
+This is a train Scheduling system that stores train data on a real-time firebase database.
+
+The application demonstrates the ability to store train arrival times and to calculate the interval arrival times using a simple algorithm. The application has an administrative menu that can be used to add and remove trains. The admin interface could easily be protected with authentication. 
+
+To Use: 
+When you start the application, if any trains are stored in the firebase data store then they will be loaded automatically and all arrival times will be updated to reflect your local time. 
+
+Administration Menu:
+To add a train simply click the 'Administration' menu located on the navigation bar.
+A dialog box will open giving you the ability to add new trains.
+To remove a train, follow the above instructions and then click the 'remove trains' button. All trains will then show a remove icon. Select the one you want to remove and it will be removed from the data base.
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -16,215 +16,305 @@
 
 
 //Initialize Firebase Database
-var config = {
-    apiKey: "AIzaSyBjJ_oHsxoGsPS7Nv1YJzdSZQ9s99QTIAc",
-    authDomain: "rpsmp-7bc44.firebaseapp.com",
-    databaseURL: "https://rpsmp-7bc44.firebaseio.com",
-    projectId: "rpsmp-7bc44",
-    storageBucket: "rpsmp-7bc44.appspot.com",
-    messagingSenderId: "106300897679"
-};
-firebase.initializeApp(config);
-//Create database references
-var database = firebase.database();
-var trainsRef = database.ref('trains');
-var trainRow = 0;
-var isAdding = false;
-var trainClockTickers = [];
-$(document).ready(function(){
+var trainTimeObj = function () {
+    var config = {
+        apiKey: "AIzaSyBjJ_oHsxoGsPS7Nv1YJzdSZQ9s99QTIAc",
+        authDomain: "rpsmp-7bc44.firebaseapp.com",
+        databaseURL: "https://rpsmp-7bc44.firebaseio.com",
+        projectId: "rpsmp-7bc44",
+        storageBucket: "rpsmp-7bc44.appspot.com",
+        messagingSenderId: "106300897679"
+    };
+    firebase.initializeApp(config);
+    //Create database references
+    var database = firebase.database();
+    var trainRow = 0;
+    var timerRef;
+    var isAdding = false;
+    var timmer;
+    var timeIcon = '<img src="assets/images/noun_clock_555527.svg">';
+    var deleteTrainIcon = '<i class="far fa-calendar-times"></i>';
+    $(document).ready(function () {
+        $('#timelogo').append(timeIcon)
 
-    database.ref('trains').on('child_added', function (snap) {
+        database.ref('trains').on('child_added', function (snap) {
 
-        if (snap) {
-            var tr = createRow(snap.val())
-        
-            $('#tableBody').append(tr)
-        }
+            if (snap) {
+                var data = snap.val()
+                data.key = snap.key
+                var tr = createRow(data)
 
-    })
-
-    $('#addit').click(function (event) {
-        $('#validation').hide()
-        $('#addit').prop('disabled',true)
-        if(!isAdding){
-            isAdding = true;
-            var trainData = makeTrainObject()
-            if(trainData !== false){
-                database.ref('trains').push(trainData)
+                $('#tableBody').append(tr)
             }
-            
-            isAdding = false;
-        } 
-        $('#addit').prop('disabled', false)
-        return
-    })
 
-    $('#trainArriveH').focusout(digithelper)
-    $('#trainArriveM').focusout(digithelper)
-    $('#frenquency').focusout(digithelper,true)
-   
+        })
 
-})
-function digithelper(event, l = false) {
-    var f = event.target.value
-    if (!l){ 
-    if (parseInt(f) < 10 && parseInt(f) > -1) {
-        f = "0" + f
-        event.target.value = f
-    } else if (parseInt(f) == -1) {
-        event.target.value = "00"
-    } else {
-        event.target.value = f
-    }
-    } else {
-        if (f < 10) {
-             event.target.value = f
-        }
-    }
+        $('#addit').click(function (event) {
+            $('#validation').hide()
+            $('#addit').prop('disabled', true)
+            if (!isAdding) {
+                isAdding = true;
+                var trainData = makeTrainObject()
+                if (trainData !== false) {
+                    database.ref('trains').push(trainData)
+                }
 
-}
-
-function nextTrain(_firstTrain, _frequency) {
-    if (hasFirstTrainArrived(_firstTrain)) {
-    
-    }
-}
-
-function makeTrainObject(){
- 
-   var trainob = {
-    trainName: $('#trainName').val(),
-    destination: $('#destination').val(),
-    trainArriveH: $('#trainArriveH').val(),
-    trainArriveM: $('#trainArriveM').val(),
-    frequency: $('#frequency').val(),
-    has1stTrainArrived: false
-    }
-    
-    if(validateForm(trainob)){
-        clearForm()
-        return trainob
-    }   
-    return false
-}
-function validateForm(_trainob){
-    var error = []
-    if (_trainob.trainName.length < 1) {
-        error.push('Train Name is missing.')
-    }
-    if (_trainob.destination.length < 1) {
-        error.push('Train destination is missing.')
-    }
-
-    var isValid = moment(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm").isValid()
-    if (!isValid) {
-        error.push('Train Arrival Time is Invalid.')
-    }
-    var isValidFre = (parseInt(_trainob.frequency) > 0) && (parseInt(_trainob.frequency) < 1440)
-    if (!isValidFre) {
-        error.push('Train Frequency is Invalid. Must be greater than 0 and less than 1,440 minutes.')
+                isAdding = false;
+            }
+            $('#addit').prop('disabled', false)
+            return
+        })
+         $('#remove').click(function (event) {
+            clearInterval(timmer)
+             $('#admintrain').modal('hide')
+              
+             $('.remove').show()
+                timerRef = setTimeout(function () {
+                 $('.remove').hide()
+                  timmer = setInterval(updateTimeofNextTrain, 1000)
+                 clearTimeout(timerRef)
+             }, 20000)
+         })
         
 
+        $('#trainArriveH').focusout(digithelperS)
+        $('#trainArriveM').focusout(digithelperS)
+        $('#frenquency').focusout(digithelperS, true)
+
+      timmer =   setInterval(updateTimeofNextTrain, 1000)
+    })
+
+    function digithelperS(event, l = false) {
+        var f = event.target.value
+        if (!l) {
+            if (parseInt(f) < 10 && parseInt(f) > -1) {
+                f = "0" + f
+                event.target.value = f
+            } else if (parseInt(f) == -1) {
+                event.target.value = "00"
+            } else {
+                event.target.value = f
+            }
+        } else {
+            if (f < 10) {
+                event.target.value = f
+            }
+        }
+
     }
-    if (error.length > 0) {
-        errors(error)
+    function digithelper(_val, l = false) {
+       
+        if (!l) {
+            if (parseInt(_val) < 10 && parseInt(_val) > -1) {
+                _val = "0" + _val
+                
+            } else if (parseInt(_val) == -1) {
+               _val = "00"
+            } else {
+                _val = _val
+            }
+        } else {
+            if (_val < 10) {
+               _val = _val
+            }
+        }
+
+        return _val
+
+    }
+
+    function makeTrainObject() {
+
+        var trainob = {
+            trainName: $('#trainName').val(),
+            destination: $('#destination').val(),
+            trainArriveH: $('#trainArriveH').val(),
+            trainArriveM: $('#trainArriveM').val(),
+            frequency: $('#frequency').val(),
+            has1stTrainArrived: false
+        }
+
+        if (validateForm(trainob)) {
+            clearForm()
+            return trainob
+        }
         return false
     }
-    return true
 
-}
-function errors(_error) {
-    
-    $('#validation').empty()
-    _error.forEach(element => {
-    $('#validation').append($('<div>').text(element))
-    });
-    $('#validation').show()
+    function validateForm(_trainob) {
+        var error = []
+        if (_trainob.trainName.length < 1) {
+            error.push('Train Name is missing.')
+        }
+        if (_trainob.destination.length < 1) {
+            error.push('Train destination is missing.')
+        }
+
+        var isValid = moment(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm").isValid()
+        if (!isValid) {
+            error.push('Train Arrival Time is Invalid.')
+        }
+        var isValidFre = (parseInt(_trainob.frequency) > 0) && (parseInt(_trainob.frequency) < 1440)
+        if (!isValidFre) {
+            error.push('Train Frequency is Invalid. Must be greater than 0 and less than 1,440 minutes.')
 
 
-}
-function clearForm(){
-    $('#trainName').val('')
-    $('#destination').val('')
-    $('#trainArriveH').val('')
-    $('#trainArriveM').val('')
-    $('#frequency').val('')
-}
-function createRow(_trainob) {
-   
-    
-    var cellName = $('<td>')
-    cellName.text(_trainob.trainName)
-    
-    var cellDest = $('<td>')
-    cellDest.text(_trainob.destination)
-    
-    var cellFirst = $('<td>')
-    if (!_trainob.trainArriveM.length) {
-        _trainob.trainArriveM = "00"
+        }
+        if (error.length > 0) {
+            errors(error)
+            return false
+        }
+        return true
+
     }
-    if (!_trainob.trainArriveH.length) {
-        _trainob.trainArriveH = "00"
+
+    function errors(_error) {
+
+        $('#validation').empty()
+        _error.forEach(element => {
+            $('#validation').append($('<div>').text(element))
+        });
+        $('#validation').show()
+
+
     }
-    var timeFirst = moment().format(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm")
-    cellFirst.text(timeFirst)
-    var firstTrainCame = moment().isSameOrAfter(moment(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm")) 
-    var cellFrequency = $('<td>')
-    cellFrequency.text(_trainob.frequency)
-    var frequency = parseFloat(_trainob.frequency * 60) // in seconds
-    console.log("Train comes every " + frequency + " seconds")
-    var cellNextTrain = $('<td>')
-    var trid ='train_' + trainRow
-    cellNextTrain.attr('id', trid)
-    var ntrainTime;
-    if (firstTrainCame) {
-        var d = new moment() // time now
-        console.log("now: " + d.format('HH:mm'))
-        var c = new moment(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm")
-        console.log("time of first train " + c.format('HH:mm'))
-        var timePastSinceFirstTrain = moment.duration(d.diff(c))
+
+    function clearForm() {
+        $('#trainName').val('')
+        $('#destination').val('')
+        $('#trainArriveH').val('')
+        $('#trainArriveM').val('')
+        $('#frequency').val('')
+    }
+
+    function createRow(_trainob) {
+
+
+        var cellName = $('<td>')
+        cellName.text(_trainob.trainName)
+
+        var cellDest = $('<td>')
+        cellDest.text(_trainob.destination)
+
+        var cellFirst = $('<td>')
+        if (!_trainob.trainArriveM.length) {
+            _trainob.trainArriveM = "00"
+        }
+        if (!_trainob.trainArriveH.length) {
+            _trainob.trainArriveH = "00"
+        }
+        var timeFirst = moment().format(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm")
+        cellFirst.text(moment(timeFirst, 'HH:mm').format('LT'))
+        // checks too see if the first train has arrived. 
+        var firstTrainCame = moment().isSameOrAfter(moment(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm"))
+        var cellFrequency = $('<td>')
+        cellFrequency.text(_trainob.frequency)
+        var frequency = parseFloat(_trainob.frequency * 60) // in seconds
+        console.log("Train comes every " + frequency + " seconds")
+        var cellNextTrain = $('<td>')
+        var trid = 'train_' + trainRow
+        cellNextTrain.attr('id', trid)
+
+        //---------------------------------------------Time Of Next Train-----------------------------------------------------//
+        //Holder of minutes till next train
+        var ntrainTime;
+        //Get cuurent time as a moment object
+        var currentTime = new moment()
+        //Convert time stored in database to a momment
+        var startTime = new moment(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "LT")
+        //Determine if the first train has arrived
+        if (firstTrainCame) {
+            //Call function to calculate remaining wait time in minutes
+            var momentofTime = timeOfNextTrain(startTime, frequency, currentTime)
+            var min = parseInt(momentofTime.asMinutes())
+            
+            var seconds = parseInt(momentofTime.asSeconds() % 60)
+            ntrainTime = digithelper(min) + ":" + digithelper(seconds)
+            if (min >= 60) {
+                var dur = moment.duration(startTime.diff(currentTime))
+                ntrainTime = dur.humanize()
+            }
+        } else {
+            var dur = moment.duration(startTime.diff(currentTime))
+            ntrainTime = dur.humanize()
+        }
+         cellNextTrain.removeClass('arrive')
+        if (seconds <= 3 && min <= 0) {
+           
+            cellNextTrain.addClass('arrive')
+            cellNextTrain.text("TRAIN IS ARRIVING")
+        } else {
+            cellNextTrain.text(ntrainTime)
+        }
+        if (timerRef) {
+           
+            var delicon = $('<td>').addClass('remove')
+            delicon.html(deleteTrainIcon).attr('id', 'remove_' + trainRow).data('key', _trainob.key)
+            var dkey = _trainob.key
+            $('#remove_' + trainRow).click(function (event) {
+                    clearTimeout(timerRef)
+                $('.remove').hide()
+               var dk =  $(this).data('key')
+                database.ref('trains/' + dk).remove()
+                $(event.target.parentNode).empty()
+                    timmer = setInterval(updateTimeofNextTrain, 1000)
+                    
+            })
+        }
+        var row = $('<tr>')
+        row.append(cellName)
+            .append(cellDest)
+            .append(cellFirst)
+            .append(cellFrequency)
+            .append(cellNextTrain)
+            .addClass('td-center')
+            .append(delicon)
+        trainRow++
+        return row
+
+    }
+
+    function timeOfNextTrain(_startTime24, _frequencyInSec, _currentTime24) {
+
+        //formula for this is 
+        // currenttime - starttime = minuetsPastSinceFirstTrain (Convert to seconds) * 60
+        // secondsPassed / the frequencey in seconds of then other trains = number of trains that have passed (The remainder gives us the fraction of how long ago the last train left) convert to seconds
+        // frequency - the above remainder = seconds left till next train
+
+        console.log("time of first train " + _startTime24.format('HH:mm'))
+
+        var timePastSinceFirstTrain = moment.duration(_currentTime24.diff(_startTime24))
         console.log("Seconds past since first train " + timePastSinceFirstTrain.asSeconds())
-        var numberOfTimesTrainHasCome = (timePastSinceFirstTrain.asSeconds() / frequency)
+
+        var numberOfTimesTrainHasCome = (timePastSinceFirstTrain.asSeconds() / _frequencyInSec)
         console.log("Time Train Has Came " + numberOfTimesTrainHasCome)
-        var remainder = (timePastSinceFirstTrain.asSeconds() % frequency)
-        console.log("Remainder " + remainder)
-        var remainderasDuration = moment.duration(remainder, 'seconds')
-        var secondsToWait = moment(d).add(remainderasDuration)
-        console.log("Minutes to wait " + secondsToWait)
-        console.log("Minutes since last train has left = " + moment.duration(remainder, 'seconds').asMinutes())
-        ntrainTime = moment.duration((frequency - remainder), 'seconds').humanize()
-        console.log("Time to wait" + ntrainTime)
-      
-    }else{
-        //how many minutes to the first trian arival time | arivalTime - nowTime 
-        var d = new moment()
-        var c = new moment(_trainob.trainArriveH + ":" + _trainob.trainArriveM, "HH:mm")
-        var dur = moment.duration(c.diff(d))
-        ntrainTime = dur.humanize()
+
+        var secondsLastTrainLeft = (timePastSinceFirstTrain.asSeconds() % _frequencyInSec)
+        console.log("Remainder number of seconds since last train left" + secondsLastTrainLeft)
+
+        console.log("Minutes since last train has left = " + moment.duration(secondsLastTrainLeft, 'seconds').asMinutes())
+        return moment.duration((_frequencyInSec - secondsLastTrainLeft), 'seconds')
+
     }
-    
-    
-    cellNextTrain.text(ntrainTime)
-    var row = $('<tr>')
-    row.append(cellName)
-        .append(cellDest)
-        .append(cellFirst)
-        .append(cellFrequency)
-        .append(cellNextTrain);
-    trainRow++
-    return row
+
+
+    function updateTimeofNextTrain() {
+        database.ref('trains').once('value',function (snap) {
+            var trains = snap
+            $('#tableBody').empty();
+            trains.forEach(function (childSnapshot) {
+                
+                var childData = childSnapshot.val();
+                
+                var tr = createRow(childData)
+                
+                 $('#tableBody').append(tr)
+            });
+          
+
+        })
+    }
 
 }
 
-function getTimeofNextTrain(_hh,_mm) {
-    var time = hhmmTimeToMoment(_hh,_mm)
-    return getTimeDiffInMinutes(time)
-}
-function hhmmTimeToMoment(_hh,_mm) {
-    return moment().format(_hh + ":" + _mm, "HH:mm");
-}
-function getTimeDiffInMinutes(_moment) {
-    return moment().diff(moment(_moment), 'minutes', true)
-}
-
-
+trainTimeObj()
