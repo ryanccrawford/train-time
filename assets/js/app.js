a229c9b1ae433e6d31b249187a7eac1cea2c4a5e
RyBo <ryanccrawford@live.com>
2019-05-18 02:49:41 -0400

    updated clock to animate with time

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,8 +35,9 @@ var trainTimeObj = function () {
     var timeIcon = '<img src="assets/images/noun_clock_555527.svg">';
     var deleteTrainIcon = '<i class="far fa-calendar-times"></i>';
     $(document).ready(function () {
-        $('#timelogo').append(timeIcon)
-
+        makeClock()
+       // $('#timelogo').append(timeIcon)
+          
         database.ref('trains').on('child_added', function (snap) {
 
             if (snap) {
@@ -318,3 +319,83 @@ var trainTimeObj = function () {
 }
 
 trainTimeObj()
+
+function makeClock() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius = radius * 0.90
+    setInterval(drawClock, 1000);
+
+
+    function drawClock() {
+        drawFace(ctx, radius);
+        drawNumbers(ctx, radius);
+        drawTime(ctx, radius);
+    }
+    function drawFace(ctx, radius) {
+        var grad;
+
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = '#6c757d';
+        ctx.fill();
+
+        grad = ctx.createRadialGradient(0, 0, radius * 1, 0, 0, radius * .90);
+        grad.addColorStop(0, '#fff');
+        grad.addColorStop(0.5, '#fff');
+        grad.addColorStop(1, '#fff');
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = radius * 0.1;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(0, 0, radius * 0.01, 0, 2 * Math.PI);
+        ctx.fillStyle = '#fff';
+        ctx.fill();
+    }
+    function drawNumbers(ctx, radius) {
+        var ang;
+        var num;
+        ctx.font = radius * 0.15 + "px arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        for (num = 1; num < 13; num++) {
+            ang = num * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -radius * 0.85);
+            ctx.rotate(-ang);
+            ctx.fillText(".".toString(), 0, 0);
+            ctx.rotate(ang);
+            ctx.translate(0, radius * 0.85);
+            ctx.rotate(-ang);
+        }
+    }
+    function drawTime(ctx, radius) {
+        var now = new Date();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds();
+        //hour
+        hour = hour % 12;
+        hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
+        drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+        //minute
+        minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+        drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+        // second
+        second = (second * Math.PI / 30);
+        drawHand(ctx, second, radius * 0.9, radius * 0.02);
+    }
+    function drawHand(ctx, pos, length, width) {
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
+    }
+}
